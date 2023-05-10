a978a43d51259c21e9852e6b004d8e46851aca22
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2023-05-10 14:42:19 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
 export default function Header({ title }) {
-  "return <h1 className="title">{title}</h1>
-  return <h2 className="title">{title}</h2>
+  return <h1 className="title">{title}</h1>
+  "return <h2 className="title">{title}</h2>
 }
