766a4ccde77475ca344e18263b05f408723fa9e3
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2025-12-07 17:07:36 -0500

    Update index.js
    TWO & Solutions inc.

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>RemoteKevin multiverse!! </title>
+        <title>TWO & Solutions inc </title>
         <link rel="icon" href="/favicon.ico" />        
       </Head>
       
