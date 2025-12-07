e4439d3441a394640f3790c6ff895116c519cefe
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2025-12-07 13:46:54 -0500

    Add copy of index.js
    Just in case uploading a copy of index.js file

---
+++ pages/index_copy.js
@@ -0,0 +1,66 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>RemoteKevin multiverse!! </title>
+        <link rel="icon" href="/favicon.ico" />        
+      </Head>
+      
+      <main>
+        <br /> 
+        <Header title="Welcome to RemoteKevin multiuniverse!!" />
+    
+           <p> Check one of YouTube shorts with a niche. Currently the accumulated views are over 125K. Enjoy watching it!</p>
+           
+           <p className="description">
+           ➤ <a href="https://youtube.com/@TWOandSolutions/">TWO & Solutions </a> YouTube channel: Science, Data, News & Climate 
+           <br />           
+            <iframe width="180" height="288" 
+            src="https://www.youtube.com/embed/6rcNDSjUiG4" 
+            title="Spectacular freshwater ice over..." 
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture; 
+            web-share" allowfullscreen>
+            </iframe>
+    
+            <iframe width="180" height="288" 
+            src="https://www.youtube.com/embed/1z0Y1c0kdqw" 
+            title="How many Microwave transmission satellite..." 
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture; 
+            web-share" allowfullscreen></iframe>
+    
+            <br />
+            <br /> 
+            <br /> 
+            ➤ <a href="https://www.youtube.com/@THREEandSolutions">THREE & Solutions </a> YouTube channel: Environment, Travel sites & Nature   
+            <br />    
+            <iframe width="180" height="288" 
+            src="https://www.youtube.com/embed/6kMNHabafi0" 
+            title="Canada geese and goslings in Silver Lake..." 
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture; 
+            web-share" allowfullscreen>
+            </iframe>
+    
+            <iframe width="180" height="288" 
+            src="https://www.youtube.com/embed/R-3K46DkkUo" 
+            title="The life of ION Light Rail Transit or LRT..." 
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture; 
+            web-share" allowfullscreen>
+            </iframe>
+            <br />
+    
+            @ 2023 by TWO & Solutions inc. All rights are reserved
+           </p>
+        
+      <Footer />
+    </main>
+    </div>
+  )
+}
