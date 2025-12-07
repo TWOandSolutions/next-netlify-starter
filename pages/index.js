e916c8d0c0397543711b1fa95297c2ac3a124db5
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2025-12-07 13:58:40 -0500

    Update index.js
    Initially it started to update new information for the website.

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
       
       <main>
         <br /> 
-        <Header title="Welcome to RemoteKevin multiuniverse!!" />
+        <Header title="Welcome to TWO & Solutions!!" />
     
-           <p> Check one of YouTube shorts with a niche. Currently the accumulated views are over 125K. Enjoy watching it!</p>
+           <p> TWO & Solutions has offered data solutions in a diverse range of computer and internet applications. </p>
            
            <p className="description">
            ➤ <a href="https://youtube.com/@TWOandSolutions/">TWO & Solutions </a> YouTube channel: Science, Data, News & Climate 
@@ -26,17 +26,11 @@ export default function Home() {
             encrypted-media; gyroscope; picture-in-picture; 
             web-share" allowfullscreen>
             </iframe>
-    
-            <iframe width="180" height="288" 
-            src="https://www.youtube.com/embed/1z0Y1c0kdqw" 
-            title="How many Microwave transmission satellite..." 
-            frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
-            encrypted-media; gyroscope; picture-in-picture; 
-            web-share" allowfullscreen></iframe>
-    
+                  
             <br />
             <br /> 
-            <br /> 
+            <br />
+              
             ➤ <a href="https://www.youtube.com/@THREEandSolutions">THREE & Solutions </a> YouTube channel: Environment, Travel sites & Nature   
             <br />    
             <iframe width="180" height="288" 
@@ -46,17 +40,11 @@ export default function Home() {
             encrypted-media; gyroscope; picture-in-picture; 
             web-share" allowfullscreen>
             </iframe>
-    
-            <iframe width="180" height="288" 
-            src="https://www.youtube.com/embed/R-3K46DkkUo" 
-            title="The life of ION Light Rail Transit or LRT..." 
-            frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
-            encrypted-media; gyroscope; picture-in-picture; 
-            web-share" allowfullscreen>
-            </iframe>
+   
+
             <br />
     
-            @ 2023 by TWO & Solutions inc. All rights are reserved
+            @ 2026 by TWO & Solutions inc. All rights are reserved
            </p>
         
       <Footer />
