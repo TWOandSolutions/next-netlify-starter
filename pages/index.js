ef4f80cd70570a2462bdcd9dd8d045f9f5825882
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2025-12-07 20:30:04 -0500

    Update index.js
    Keep editing

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,12 @@ export default function Home() {
 
            <p> TWO & Solutions has offered data solutions in a diverse range of computer and internet applications. </p>
 
-           <p> Two & Solutions offers data science solutions and consulting services with over 30-year experience 
-               from industry and academic experience, project, and knowledge in diverse institutions and companies. </p>
+           <p> Two & Solutions offers data science solutions and consulting services with over 30-year experience   </p>
+           <p> from industry and academic experience, project, and knowledge in diverse institutions and companies. </p>
             
            <p className="description">
-           ➤ <p> We provide appropriate guidance for beginner, intermediate users and clients to be ready for working independently 
-                  at the next level of work from real world problems in the future - guiding solutions at your areas and disciplinary. </p>
+           ➤ <p> We provide appropriate guidance for beginner, intermediate users and clients to be ready for working independently   </p>
+              <p> at the next level of work from real world problems in the future - guiding solutions at your areas and disciplinary. </p>
            
            </p>
         
