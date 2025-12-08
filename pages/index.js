4ebe4d70edf53a26df91b9c67efb853ba8c54c64
TWO&solutions <59812127+TWOandSolutions@users.noreply.github.com>
2025-12-07 20:21:31 -0500

    Update index.js
    Adding two paragraphs in the body

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,10 @@ export default function Home() {
         <Header title="Welcome to TWO & Solutions!!" />
 
            <p> TWO & Solutions has offered data solutions in a diverse range of computer and internet applications. </p>
+
+           <p> Two & Solutions offers data science solutions and consulting services with over 30-year experience from industry and academic experience, project, and knowledge in diverse institutions and companies. </p>
+
+           <p> We provide appropriate guidance for beginner, intermediate users and clients to be ready for working independently at the next level of work from real world problems in the future - guiding solutions at your areas and disciplinary. </p>    
     
            <p className="description">
            ➤ The website is under construction now. 
